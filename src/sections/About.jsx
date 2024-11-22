c048b3455c34e611f35e3fc8e37caea307c5c71e
abdullah.odeh <abdullah.odeh@wunderkind.co>
2024-11-22 07:54:28 -0800

    customizing portfolio

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -23,10 +23,9 @@ const About = () => {
             <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-headtext">Hi, I’m Abdullah Odeh</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+                I'm a professional Web Developer with over 4 years of experience. I currently work at Wunderkind.
               </p>
             </div>
           </div>
@@ -39,8 +38,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                I specialize in a variety of languages and frameworks, primarily React, NextJS, Headless CMS, and GraphQL.
               </p>
             </div>
           </div>
@@ -62,8 +60,8 @@ const About = () => {
               />
             </div>
             <div>
-              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <p className="grid-headtext">I’m open to working from anywhere in the world.</p>
+              <p className="grid-subtext">I&apos;m based in Santa Monica, California.</p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
@@ -95,7 +93,7 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">adrian@jsmastery.pro</p>
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">abdullahsayshello@gmail.com</p>
               </div>
             </div>
           </div>
