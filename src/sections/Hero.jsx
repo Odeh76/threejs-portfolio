7ac6885056cd7f3bbd5797bcb32505a3348c1814
abdullah.odeh <abdullah.odeh@wunderkind.co>
2024-11-22 12:12:43 -0800

    minor content update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I'm Abdullah <span className="waving-hand">👋</span>
+          Hello, I'm Abdullah <span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">I build things on the web.</p>
       </div>
